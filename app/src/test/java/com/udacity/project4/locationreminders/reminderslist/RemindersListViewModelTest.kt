e102d4c1ee4595325b8893eb8b31fe52518d6a8d
Ghazouly <maguedfathey@gmail.com>
2022-12-25 08:02:41 +0200

    20 commit

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -3,11 +3,18 @@ package com.udacity.project4.locationreminders.reminderslist
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import com.udacity.project4.locationreminders.MainCoroutineRule
 import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.pauseDispatcher
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 //provide testing to the RemindersListViewModel and its live data objects
 @RunWith(AndroidJUnit4::class)
@@ -36,6 +43,65 @@ class RemindersListViewModelTest {
 
         viewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(),repository)
     }
+
+    @After
+      fun tearDown(){ stopKoin()  }
+
+
+    @Test
+      fun loadReminders_loading(){
+          mainCoroutineRule.pauseDispatcher()
+
+          viewModel.loadReminders()
+          assertThat(viewModel.showLoading.getOrAwaitValue()).isTrue()
+
+
+
+
+
+
+
+
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
@@ -57,4 +123,3 @@ class RemindersListViewModelTest {
 
 
 
-}
